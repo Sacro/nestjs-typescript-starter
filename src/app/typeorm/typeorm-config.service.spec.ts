79816af99467aaa82f95c7140c54bfd48bdee45f
Ben Woodward <ben@benwoodward.me.uk>
2019-01-25 14:02:09 +0000

    Add personal base code

---
+++ src/app/typeorm/typeorm-config.service.spec.ts
@@ -0,0 +1,55 @@
+import 'jest-extended';
+
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigService } from '../config/config.service';
+import { TypeOrmConfigService } from './typeorm-config.service';
+
+describe('TypeormConfigService', () => {
+  let config: ConfigService;
+  let service: TypeOrmConfigService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ConfigService, TypeOrmConfigService],
+    }).compile();
+    config = module.get<ConfigService>(ConfigService);
+    service = module.get<TypeOrmConfigService>(TypeOrmConfigService);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return a dev config object', () => {
+    jest
+      .spyOn(config, 'isDevEnvironment', 'get')
+      .mockImplementation(() => true);
+    const options = service.createTypeOrmOptions();
+
+    expect(options.logging).toBe('all');
+    expect(options.entities[0]).toStartWith('src');
+  });
+
+  it('should return a test config object', () => {
+    const options = service.createTypeOrmOptions();
+
+    expect(options.logging).toEqual(['error']);
+    expect(options.entities[0]).toStartWith('src');
+  });
+
+  it('should return a production config object', () => {
+    jest
+      .spyOn(config, 'isProdEnvironment', 'get')
+      .mockImplementation(() => true);
+
+    const options = service.createTypeOrmOptions();
+
+    expect(options.logging).toEqual(['error']);
+    expect(options.entities[0]).toStartWith('dist');
+  });
+});
---
+++ wallaby.js
@@ -0,0 +1,30 @@
+process.env.NODE_ENV = 'test';
+
+module.exports = function() {
+  return {
+    env: {
+      runner: 'node',
+      type: 'node',
+    },
+    files: [
+      'sample.env',
+      'tsconfig.json',
+      'package.json',
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/main.ts', ignore: true },
+      { pattern: 'src/main.hmr.ts', ignore: true },
+      { pattern: 'src/**/*.json', ignore: true },
+      { pattern: 'src/**/*.module.ts', ignore: true },
+      { pattern: 'src/**/*.spec.ts', ignore: true },
+    ],
+    reportConsoleErrorAsError: true,
+    setup: function(wallaby) {
+      var jestConfig = require('./package.json').jest;
+      // for example:
+      // jestConfig.globals = { "__DEV__": true };
+      wallaby.testFramework.configure(jestConfig);
+    },
+    testFramework: 'jest',
+    tests: [{ pattern: 'src/**/*spec.ts' }],
+  };
+};
