f2d5a59c4ba51d0f3544ef0d6bbd76e5d2e9906e
Ben Woodward <ben@benwoodward.me.uk>
2019-08-09 20:46:41 +0100

    Don't actually add health stuff, just endpoint

--- src/app/terminus/terminus-options.service.ts
+++ src/app/terminus/terminus-options.service.ts
@@ -1,23 +1,18 @@
 import {
   TerminusEndpoint,
   TerminusOptionsFactory,
-  DNSHealthIndicator,
   TerminusModuleOptions,
-  HealthIndicatorResult,
 } from '@nestjs/terminus';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TerminusOptionsService implements TerminusOptionsFactory {
-  public constructor(private readonly dns: DNSHealthIndicator) {}
+  public constructor() {}
 
   public createTerminusOptions(): TerminusModuleOptions {
     const healthEndpoint: TerminusEndpoint = {
       url: '/health',
-      healthIndicators: [
-        async (): Promise<HealthIndicatorResult> =>
-          this.dns.pingCheck('google', 'https://www.google.com'),
-      ],
+      healthIndicators: [],
     };
     return {
       endpoints: [healthEndpoint],
