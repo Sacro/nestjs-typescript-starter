a845d515600d09c7c58f4567848dbc5e2ddb59b9
Ben Woodward <ben@benwoodward.me.uk>
2021-10-12 11:24:24 +0100

    Move wallaby config

---
+++ wallaby.conf.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = () => {
+  return {
+    autodetect: true,
+
+    // modify `files` automatic configuration settings
+    files: {
+      override: (filePatterns) => {
+        // TODO: modify `filePatterns` array as required
+        return filePatterns;
+      },
+    },
+
+    // modify `tests` automatic configuration settings
+    tests: {
+      override: (testPatterns) => {
+        // TODO: modify `testPatterns` array as required
+        return testPatterns;
+      },
+    },
+    // env: {
+    //   runner: 'node',
+    //   type: 'node',
+    // },
+    // files: [
+    //   'sample.env',
+    //   'tsconfig.json',
+    //   'package.json',
+    //   { pattern: 'src/**/*.ts', load: false },
+    //   { pattern: 'src/main.ts', ignore: true },
+    //   { pattern: 'src/**/app.module.ts', ignore: true },
+    //   { pattern: 'src/**/*.json', ignore: true },
+    //   { pattern: 'src/**/*.spec.ts', ignore: true },
+    // ],
+    // reportConsoleErrorAsError: true,
+    // setup: function (wallaby) {
+    //   // eslint-disable-next-line @typescript-eslint/no-var-requires,unicorn/prefer-module
+    //   let jestConfig = require('./package.json').jest;
+    //   // for example:
+    //   // jestConfig.globals = { "__DEV__": true };
+    //   wallaby.testFramework.configure(jestConfig);
+    // },
+    // testFramework: 'jest',
+    // tests: [{ pattern: 'src/**/*spec.ts' }],
+  };
+};
